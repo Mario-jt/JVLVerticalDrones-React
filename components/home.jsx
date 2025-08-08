3cc19a94ff51fd0ce5937980b9254e2f28a3ab05
Mario <mariojt15@gmail.com>
2025-08-08 16:28:36 -0300

    versao 2.2

--- components/home.jsx
+++ components/home.jsx
@@ -29,7 +29,7 @@ const Menu = () => {
           </li>
         </ul>
       </nav>
-      <a href="https://wa.me/553192116990?text=Olá,%20vim%20pelo%20seu%20site%20e%20gostaria%20de%20um%20orçamento" target="_blank" rel="noopener noreferrer" className="nav-tell">
+      <a href="https://wa.me/553192116990?text=Olá,%20VerticalDrones!%20Entrei%20em%20contato%20através%20do%20seu%20site%20e%20gostaria%20de%20solicitar%20um%20orçamento%20para%20os%20serviços%20de%20mapeamento%20com%20drones." target="_blank" rel="noopener noreferrer" className="nav-tell">
         Solicite um Orçamento
       </a>
     </header>
